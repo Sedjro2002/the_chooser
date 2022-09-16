817f69415e215151afc4f489f2d59b7deb9a0cf4
Haricrim <habibhbn3@gmail.com>
2022-09-16 10:51:39 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+from random import *
+
+ans=input("do you want too see anime ?")
+animes=["naruto", "jjk", "demonSlayer"]
+
+if ans=="y":
+    print("ok\nwatch this:")
+    print(animes[randint(0,2)])
+else:
+    print('byyye!')
+
+
+
