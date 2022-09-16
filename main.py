ca79aab8c1b1547eb5f5445b156889e6f079516b
Haricrim <habibhbn3@gmail.com>
2022-09-16 13:35:00 +0100

    the_chooser_v1

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from random import *
 
 ans=input("do you want too see anime ?")
-animes=["naruto", "jjk", "demonSlayer"]
+
+animes={
+    "action":["naruto","one piece","bleach","dragon ball"],
+    "comedy":["gintama","one punch man","le college foufoufou"],
+    "drama":["your name","ousamma ranking"]
+}
 
 if ans=="y":
+    print("what type of anime do you want to see ?\nThe disponible types are: ")
+    for x in animes:
+        print(x)
+    typeofanimes=input("your choice : ")
     print("ok\nwatch this:")
-    print(animes[randint(0,2)])
+    print(animes[typeofanimes][randint(0,len(animes[typeofanimes])-1)])
 else:
     print('byyye!')
-
+    
+    
+    
 
 
